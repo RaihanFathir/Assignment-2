a4bc65b3d847ebec4bc57810c0b14edcbaa2937b
RaihanFathir <raihan.fathir_ti25@nusaputra.ac.id>
2026-03-09 18:09:52 +0700

    Add files via upload

---
+++ LamaPembelianLaptopdalamBulan.py
@@ -0,0 +1,6 @@
+Harga_laptop = (int(input("Masukan harga laptop(Dalam rupiah):Rp.")))
+Alokasi_perbulan = (int(input("Alokasi dana per-bulan:Rp.")))
+
+lama_bulan = Harga_laptop / Alokasi_perbulan 
+
+print("Total bulan yang dibutuhkan:", lama_bulan, "bulan")
